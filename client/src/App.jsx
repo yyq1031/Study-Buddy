b760031ca455ebd0772ac34c8f155116b01bd031
Darrell <loyemoye110@gmail.com>
2025-06-16 15:26:10 +0800

    Lesson Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import SignIn from './pages/SignIn';
 import NavigationBar from './components/NavigationBar';
 import AccountPage from './pages/Account';
+import Lessons from './pages/Lessons';
 
 function App() {
   return(
@@ -14,10 +15,11 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About/>} />
         <Route path="/signin" element={<SignIn />} />
-         <Route path="/account" element={<AccountPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/lessons" element={<Lessons />} />
       </Routes>
     </Router>
   )
 }
 
-export default App
+export default App;
