14f81201b46e503a85b1352428827fa650b46e34
FionaQY <fionaandfauna@gmail.com>
2025-06-17 22:05:26 +0800

    remove test

--- client/src/api.js
+++ client/src/api.js
@@ -1,18 +1,14 @@
 import app from "./firebase";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const base = import.meta.env.VITE_API_BASE_URL;
-export const hello_api = `${base}/hello`;
-export const signup_api = `${base}/singup`;
-export const getprofile_api = `${base}/getprofile`
-export const getclasses_api = `${base}/getClasses`
+const base = 'http://localhost:5001/api';
 
 export const signUp = async (name, email, password) => {
   const auth = getAuth(app);
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const idToken = await userCredential.user.getIdToken();
-    const response = await fetch(getprofile_api, {
+    const response = await fetch(`${base}/getprofile`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${idToken}`,
@@ -38,7 +34,7 @@ export const login = async (email, password) => {
   const userCredential = await signInWithEmailAndPassword(auth, email, password);
   const idToken = await userCredential.user.getIdToken();
   
-  const response = await fetch(getprofile_api, {
+  const response = await fetch(`${base}/getprofile`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${idToken}`,
@@ -68,7 +64,7 @@ export const profile = async (idToken) => {
 
 export const getClasses = async (idToken) => {
   try {
-    const response = await fetch(getclasses_api, {
+    const response = await fetch(`${base}/getClasses`, {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${idToken}`,
